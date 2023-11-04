8340ab9e7b360ba01d7981d26abbff46f64ab6b2
Alexey Masyukov <alexmasyukov@gmail.com>
2023-11-04 08:54:10 +0400

    autoscroll answer history

--- src/components/AnswerHistory/AnswerHistory.tsx
+++ src/components/AnswerHistory/AnswerHistory.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import clsx from 'clsx';
 import ScrollContainer from 'react-indiana-drag-scroll';
 import { AnswerHistory as AnswerHistoryType, QuestionIndex } from '../../types';
@@ -10,251 +10,45 @@ interface Props {
 }
 
 const AnswerHistory: React.FC<Props> = ({ answerHistory, activeQuestionIndex, onSelectQuestion }) => {
+  const scrollContainer = useRef<HTMLDivElement>(null);
+  const numbersContainer = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    if (numbersContainer.current) {
+      const activeNumber = numbersContainer.current.querySelector('.last');
+      if (activeNumber) {
+        activeNumber.scrollIntoView({ behavior: 'smooth', block: 'end' });
+      }
+    }
+  }, [activeQuestionIndex]);
+
   return (
     <div className='answer-history'>
-      <ScrollContainer>
-        <ul className='question-numbers'>
+      <button
+        onClick={() => {
+          if (scrollContainer.current) {
+            scrollContainer.current.scrollTo(400, 0);
+          }
+        }}
+      >
+        Scroll to random
+      </button>
+      <ScrollContainer innerRef={scrollContainer}>
+        <ul ref={numbersContainer} className='question-numbers'>
           {answerHistory.map((item, index) => (
-            <>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-              <li
-                key={index}
-                className={clsx({ active: activeQuestionIndex === item.questionIndex }, item.status)}
-                onClick={onSelectQuestion(index)}
-              >
-                {index + 1}
-              </li>
-            </>
+            <li
+              key={index}
+              className={clsx(
+                {
+                  active: item.questionIndex === activeQuestionIndex,
+                  last: item.questionIndex === activeQuestionIndex + 4,
+                },
+                item.status
+              )}
+              onClick={onSelectQuestion(index)}
+            >
+              {index + 1}
+            </li>
           ))}
         </ul>
       </ScrollContainer>
