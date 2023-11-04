40ce1ac8e40d87824c68a89d3304f2953c997811
Alexey Masyukov <alexmasyukov@gmail.com>
2023-11-04 22:59:54 +0400

    i18n support

--- src/components/AnswerHistory/AnswerHistory.tsx
+++ src/components/AnswerHistory/AnswerHistory.tsx
@@ -14,25 +14,47 @@ const AnswerHistory: React.FC<Props> = ({ answerHistory, activeQuestionIndex, on
   const numbersContainer = useRef<HTMLUListElement>(null);
 
   useEffect(() => {
+    const center = Math.round(382 / 39 / 2) + 1;
+    console.log(center);
+
     if (numbersContainer.current) {
-      const activeNumber = numbersContainer.current.querySelector('.move-to');
+      // const activeNumber = numbersContainer.current.querySelector(`.index-${activeQuestionIndex}`);
+      const activeNumber = numbersContainer.current.querySelector(`.active`);
       if (activeNumber) {
-        activeNumber.scrollIntoView({ behavior: 'smooth' }); //, block: 'start'
+        console.log(activeNumber);
+        const left = activeNumber.getBoundingClientRect().x;
+        console.log(left);
+
+        if (activeQuestionIndex > center) {
+          scrollContainer.current?.scrollTo({
+            left: (activeQuestionIndex - (center - 1)) * 39,
+
+            behavior: 'smooth',
+          });
+        }
+
+        // activeNumber.scrollIntoView({ behavior: 'smooth', inline: 'end', block: 'end' }); //,
+        // activeNumber.getBoundingClientRect()
+        // scrollContainer.current?.scrollTo({
+        //   left: activeNumber.getBoundingClientRect().left + activeNumber.getBoundingClientRect().width + 5,
+        //   behavior: 'smooth',
+        // });
       }
     }
   }, [activeQuestionIndex]);
 
   return (
     <div className='answer-history'>
-      <ScrollContainer innerRef={scrollContainer}>
+      <ScrollContainer innerRef={scrollContainer} vertical={false}>
         <ul ref={numbersContainer} className='question-numbers'>
           {answerHistory.map((item, index) => (
             <li
               key={index}
               className={clsx(
+                `index-${activeQuestionIndex}`,
                 {
                   active: item.questionIndex === activeQuestionIndex,
-                  'move-to': item.questionIndex === activeQuestionIndex + 2,
+                  'move-to': item.questionIndex === activeQuestionIndex + 1,
                 },
                 item.status
               )}
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,35 @@
 import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
-const Header: FC = () => (
-  <header>
-    <nav>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/groups'>Groups</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+const Header: FC = () => {
+  const { i18n } = useTranslation();
+
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/groups'>Groups</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <button type='button' onClick={() => changeLanguage('ru')}>
+        ru
+      </button>
+      <button type='button' onClick={() => changeLanguage('am')}>
+        am
+      </button>
+    </header>
+  );
+};
 
 export default Header;
