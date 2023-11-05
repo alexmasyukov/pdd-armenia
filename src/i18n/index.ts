236ce16f51c6c20ba7aa28d633ad8494c59efe4d
Alexey Masyukov <a.masyukov@arenadata.io>
2023-11-05 06:00:10 +0400

    save default lang after refresh

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -12,6 +12,7 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: 'ru',
+    lng: localStorage.getItem('lang') || 'ru',
     debug: true,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
