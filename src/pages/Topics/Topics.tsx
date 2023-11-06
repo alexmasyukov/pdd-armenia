0737c659c3acbb32282998905bba2cc78d1debef
Alexey Masyukov <a.masyukov@arenadata.io>
2023-11-07 03:56:11 +0400

    Topics page with links

--- src/pages/Topics/Topics.tsx
+++ src/pages/Topics/Topics.tsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import Grid from '@mui/material/Grid';
 import { getEmptyBaseData } from '../../helpers';
 import { routes } from '../../router/constants';
-import { BaseData } from '../../types';
+import { BaseData, Group } from '../../types';
+import s from './Topics.module.scss';
+
+const getQestionCount = (groupId: Group['id'], allQestions: BaseData['questions']) => {
+  return allQestions.filter(({ gid }) => gid === groupId).length;
+};
 
 const Topics: React.FC = () => {
+  const { t, i18n } = useTranslation();
   const [data, setData] = useState<BaseData>(getEmptyBaseData());
   const [loading, setLoading] = useState<boolean>(true);
 
@@ -19,13 +27,44 @@ const Topics: React.FC = () => {
     <>Loaing</>
   ) : (
     <>
-      <ul>
-        {data.groups.map((group) => (
-          <li>
-            <Link to={routes.topics.topicById.view(group.id)}>{group.name}</Link>
-          </li>
-        ))}
-      </ul>
+      <Grid container className={s.topicsTitle}>
+        <Grid item xs={9} sm={10} md={11}>
+          {t('topic')}
+        </Grid>
+        <Grid item xs={3} sm={2} md={1} textAlign={'right'}>
+          <div className={s.count}>
+            <div>{t('solved')}</div>
+            <div>{t('total')}</div>
+          </div>
+        </Grid>
+      </Grid>
+      {data.groups.map((group, index) => (
+        <>
+          <Grid container spacing={1} className={s.topics}>
+            <Grid item xs={9} sm={10} md={11}>
+              <Link to={routes.topics.topicById.view(group.id)}>{group.name}</Link>
+            </Grid>
+            <Grid item xs={3} sm={2} md={1} textAlign={'right'}>
+              <div className={s.count}>
+                <div>
+                  <span>0</span>
+                  {' '}/{' '}
+                  <span>0</span>
+                </div>
+                <div>{getQestionCount(group.id, data.questions)}</div>
+              </div>
+            </Grid>
+            <Grid item xs={12}>
+              <div className={s.progress}></div>
+            </Grid>
+            {index !== data.groups.length - 1 && (
+              <Grid item xs={12}>
+                <div className={s.hr} />
+              </Grid>
+            )}
+          </Grid>
+        </>
+      ))}
     </>
   );
 };
