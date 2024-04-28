d34d3d4cc6c06c59758eb42921a42ee1d4825279
Alexey Masyukov <alexmasyukov@gmail.com>
2024-04-28 22:23:11 +0400

    fixes and updates

--- src/components/ShowRightAnswersBtn/ShowRightAnswersBtn.tsx
+++ src/components/ShowRightAnswersBtn/ShowRightAnswersBtn.tsx
@@ -5,7 +5,10 @@ const ShowRightAnswersBtn = () => {
   const { showRightAnswers, changeShowRightAnswersFlag } = useSettings();
 
   return (
-    <div className='favorite-btn report-btn' onClick={changeShowRightAnswersFlag}>
+    <div
+      className={`favorite-btn report-btn show-right-answer-btn ${showRightAnswers && 'active'}`}
+      onClick={changeShowRightAnswersFlag}
+    >
       {showRightAnswers ? (
         <>
           <PiEyeSlashLight size={16} /> Не отображать правильные ответы
